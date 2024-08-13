f8155d22bfa90f9e5e5553e039466024a29bf24f
fortunekenny <gbephraim@gmail.com>
2024-08-13 21:05:48 +0100

    commit2

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,6 @@
+const layout = ({children}:{children:React.ReactNode}) => {
+  return (
+    <div>{children}</div>
+  )
+}
+export default layout
---
+++ middleware.ts
@@ -0,0 +1,36 @@
+
+
+/*import { authMiddleware } from "@clerk/nextjs";
+
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+export default authMiddleware({
+  publicRoutes: ["/"],
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};*/
+
+
+
+
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+
+const isProtectedRoute = createRouteMatcher(['/add-job','/jobs(.*)','/stats']);
+
+ 
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
+
+ 
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
