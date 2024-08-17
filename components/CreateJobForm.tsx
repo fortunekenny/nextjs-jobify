34b21a08089704c132db49d5f432eb1d57a1c731
fortunekenny <gbephraim@gmail.com>
2024-08-17 15:54:15 +0100

    jobs-page setup

--- components/CreateJobForm.tsx
+++ components/CreateJobForm.tsx
@@ -2,18 +2,14 @@
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-
-import {
-  JobStatus,
-  JobMode,
-  createAndEditJobSchema,
-  CreateAndEditJobType,
-} from "@/utils/types";
-
+import { JobStatus, JobMode, createAndEditJobSchema, CreateAndEditJobType} from "@/utils/types";
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
-
 import { CustomFormField, CustomFormSelect } from "./FormComponents";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createJobAction } from "@/utils/action";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
 
 function CreateJobForm() {
   // 1. Define your form.
@@ -28,10 +24,34 @@ function CreateJobForm() {
     },
   });
 
+  const queryClient = useQueryClient();
+  const { toast } = useToast();
+  const router = useRouter();
+  const { mutate, isPending } = useMutation({
+    mutationFn: (values: CreateAndEditJobType) => createJobAction(values),
+    onSuccess: (data) => {
+      if (!data) {
+        toast({
+          description: "there was an error",
+        });
+        return;
+      }
+      toast({ description: "job created" });
+      queryClient.invalidateQueries({ queryKey: ["jobs"] });
+      queryClient.invalidateQueries({ queryKey: ["stats"] });
+      queryClient.invalidateQueries({ queryKey: ["charts"] });
+
+      router.push("/jobs");
+      // form.reset();
+    },
+  });
+
+
   function onSubmit(values: CreateAndEditJobType) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values);
+    mutate(values);
   }
 
   return (
@@ -64,12 +84,12 @@ function CreateJobForm() {
             items={Object.values(JobMode)}
           />
 
-          <Button type="submit" className="self-end capitalize">
-            create job
+          <Button type="submit" className="self-end capitalize" disabled={isPending}>
+            {isPending ? "loading..." : "create job"}
           </Button>
         </div>
       </form>
     </Form>
-  );
+  )
 }
-export default CreateJobForm;
+export default CreateJobForm
